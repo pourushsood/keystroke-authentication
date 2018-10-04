49041428d1ec89fac32196e304b80c151e6b4a19
Manan Khaneja <manankhaneja@yahoo.com>
2018-10-04 20:21:24 +0530

    Updated concat.py

--- File concatenation/concat.py
+++ File concatenation/concat.py
@@ -1,37 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  2 21:52:24 2018
+Created on Thu Oct  4 18:26:00 2018
 
 @author: Manan Khaneja
 """
+
 import fileinput
 import os
 
 """
 Expected input data format : Takes in the path of the master directory.  
-|-- Master directory (you can put any name for this) (Put the data of all the weeks in one place)
-    |-- week 1
-        |-- (your roll number)
-            |-- the same directory structure as for data submission. 
-    |-- week 2
-    |-- week 3
+|-- Master directory (you can put any name for this) (Put the data of all the weeks in one place under the roll number. And have directories of all the roll numbers)
+    |--rollnumber.txt ( a txt file with one roll number in each line)
+    |--(roll number 1)
+        |-- week 1
+            |-- (roll number 1)
+                |-- the same directory structure as for data submission. 
+        |-- week 2
+        |-- week 3
+    |--(roll number 2)
+    |--(roll number 3)
     and so on.
 
-Output data format : 
+output format : 
 |-- Master directory
-    |-- (your roll no.)
-        |-- happy.txt
-        |-- sad.txt
-        |-- neutral.txt
-    |-- week 1 
-    |-- week 2
-    |-- week 3
+    |--output
+        |--(roll number 1)
+        |--(roll number2)
+            |-- happy.txt
+            |-- sad.txt
+            |-- neutral.txt
+    |-- (roll number 1)
+    |-- (roll number 2)
     and so on.
 
 """
+
 # A function that concatinates all the files in a given directory with path dir_path
 # outpath is path where output file should be stored. outfile_name is the name of file.  
-def concatination(dir_path, outpath, outfile_name):
+def concatenation(dir_path, outpath, outfile_name):
     outfile_path = outpath + "\\" + outfile_name
     filenames = os.listdir(dir_path)
     complete_names=[]
@@ -43,29 +50,38 @@ def concatination(dir_path, outpath, outfile_name):
             fout.write(line)
 
 def main():
-    roll = input("Enter your roll number \n")
     user_input =input("Enter the complete path of your master directory:  \n")
     assert os.path.isdir(user_input), "I did not find the directory"
     print("\n")
-    all_files=os.listdir(user_input)
-    cmd = "mkdir " + user_input + "\\" + roll
-    os.system(cmd)
-    outpath = user_input + "\\" + roll 
     
-    for index in range(len(all_files)):
-    #loop on all the weeks
-        if "week" or "Week" in all_files[index]:
-            address = user_input + "\\" + all_files[index] + "\\" + roll + "\\" + "New Data Collection" 
-            directory = os.listdir(address)
-            address = address + "\\" + directory[0] + "\\" + "Keyboard Database" + "\\" + "sentence" 
-            # getting the addresses of happy, sad and neutral directories.
-            address_happy = address + "\\" + "Emotional" + "\\" + "Happy"
-            address_sad = address + "\\" + "Emotional" + "\\" + "Sad"
-            address_neutral = address + "\\" + "Neutral"
-            # appending happy.txt, sad.txt, neutral.txt with the data in "dir_path" address_happy, address_sad, address_neutral respectively. 
-            concatination(address_happy, outpath, "happy.txt")
-            concatination(address_sad, outpath, "sad.txt")
-            concatination(address_neutral, outpath, "neutral.txt")
+    cmd = "mkdir " + user_input + "\\" + "output"
+    os.system(cmd)
+    outdatapath = user_input + "\\" + "output"
+    with open(user_input + "\\" + "rollnumber.txt") as f:
+        rollnumbers = f.readlines()
+    rollnumbers = [x.strip() for x in rollnumbers]
+    for i in range(len(rollnumbers)):
+    #loop on all the roll numbers
+        roll = rollnumbers[i]
+        cmd = "mkdir " + outdatapath + "\\" + roll
+        os.system(cmd)
+        outpath = outdatapath + "\\" + roll 
+        all_files=os.listdir(user_input + "\\" + roll)
+        for index in range(len(all_files)):
+        #loop on all the weeks
+            if "week" or "Week" in all_files[index]:
+                address = user_input + "\\" + roll + "\\" + all_files[index] + "\\" + roll + "\\" + "New Data Collection" 
+                directory = os.listdir(address)
+                address = address + "\\" + directory[0] + "\\" + "Keyboard Database" + "\\" + "sentence" 
+                # getting the addresses of happy, sad and neutral directories.
+                address_happy = address + "\\" + "Emotional" + "\\" + "Happy"
+                address_sad = address + "\\" + "Emotional" + "\\" + "Sad"
+                address_neutral = address + "\\" + "Neutral"
+                # appending happy.txt, sad.txt, neutral.txt with the data in "dir_path" address_happy, address_sad, address_neutral respectively. 
+                concatenation(address_happy, outpath, "happy.txt")
+                concatenation(address_sad, outpath, "sad.txt")
+                concatenation(address_neutral, outpath, "neutral.txt")
             
 if __name__=='__main__':
     main()
+    
